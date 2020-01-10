4c27527fd9ceff11dac2ae7408eb1956cf5979b2
Aryan Singh <aryan.major@gmail.com>
2020-01-10 19:09:39 +0530

    feat(admin): admin end

---
+++ public/admin/precache-manifest.052286885b8160b04a324ccad3787aec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7b788d030fccfb890e17d4c8f6c0e1f",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "a06cce7ced343e30236d",
+    "url": "/admin/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b0b2600cdef2488d2005",
+    "url": "/admin/static/js/2.54e5dad5.chunk.js"
+  },
+  {
+    "revision": "5ba8ceb64eea848335a496f62ab9063a",
+    "url": "/admin/static/js/2.54e5dad5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a06cce7ced343e30236d",
+    "url": "/admin/static/js/main.398ee7ac.chunk.js"
+  },
+  {
+    "revision": "e091aabd6a6f09b6de8e",
+    "url": "/admin/static/js/runtime-main.213b0d45.js"
+  }
+]);
